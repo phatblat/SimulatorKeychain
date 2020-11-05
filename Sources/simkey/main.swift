4bb671a464440948b4d4a782ce3c52735ac8ac28
Ben Chatelain <ben@octop.ad>
2020-11-05 08:03:23 -0700

    ✨ Build path to db file

--- Sources/simkey/main.swift
+++ Sources/simkey/main.swift
@@ -1,3 +1,15 @@
-struct SimulatorKeychain {
-    var text = "Hello, World!"
-}
+import Path
+import SQLite
+
+let device = "2BB0BA90-51E2-4966-8A91-5A22E8618353"
+let keychain = "keychain-2-debug.db"
+
+let path = Path.home/"Library/Developer/CoreSimulator/Devices"/device/"data/Library/Keychains"/keychain
+print(path)
+
+let bundleID = "MTGSZH8QM4.co.log-g.RealmTaskTracker"
+
+let db = try Connection(path.string)
+
+// genp table, data column
+// where agrp = bundleID
